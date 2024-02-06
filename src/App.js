789e06d189c62596514b6a223374a872767ca712
Shilpa <shilpa45285@gmail.com>
2024-02-06 11:09:06 +0530

    imports updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import "./App.css";
 import Layout from "./components/layout/Layout";
-import LogIn from "./pages/login/LogIn";
-import Register from "./pages/register/Register";
 
 function App() {
   return (
